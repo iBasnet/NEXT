e4d7d8315b44fa6aafcde58c201d59a7b4d912ac
naive <iampratikbasnet@gmail.com>
2025-02-06 17:52:13 +0545

    working full fledge prototype

--- todo/src/app/page.tsx
+++ todo/src/app/page.tsx
@@ -35,6 +35,22 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Label } from "@/components/ui/label"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Switch } from "@/components/ui/switch"
 
 
 export default function page() {
@@ -42,10 +58,12 @@ export default function page() {
   let sampleTodo = {
     id: Date.now(),
     desc: "Learn NEXT.js thoroughly 〽️",
+    isComplete: false,
   }
 
   const [todos, setTodos] = useState([sampleTodo]);
   const [inputValue, setInputValue] = useState('');
+  const [isEditOpen, setIsEditOpen] = useState(false);
 
   const { toast } = useToast()
 
@@ -60,12 +78,64 @@ export default function page() {
       return;
     }
 
-    setTodos(prevArr => [...prevArr, { id: Date.now(), desc: inputValue }]);
+    setTodos(prevArr => [...prevArr, {
+      id: Date.now(),
+      desc: inputValue,
+      isComplete: false
+    }]);
     setInputValue('');
   }
 
   const deleteTodo = (id: number) => {
     setTodos(prevArr => prevArr.filter(todo => todo.id !== id));
+  }
+
+  const handleEditIcon = (id: number) => {
+
+    setIsEditOpen(true);
+
+    const currentTodo = todos.find(todo => todo.id === id);
+
+    if (!currentTodo) {
+      toast({
+        title: "⚠️ Something went wrong!",
+        description: "🏹 Todo not found.",
+      })
+      return;
+    }
+
+    setInputValue(currentTodo.desc);
+  }
+
+  const handleEditSave = (id: number) => {
+
+    setTodos(prevTodos =>
+      prevTodos.map(todo =>
+        todo.id === id ? { ...todo, desc: inputValue } : todo
+      )
+    );
+
+    setIsEditOpen(false);
+    setInputValue('');
+  }
+
+  const handleSwitch = (id: number) => {
+
+    setTodos((prevTodos) => (
+      prevTodos.map((todo) => (
+        todo.id === id ? { ...todo, isComplete: !todo.isComplete } : todo
+      ))
+    ))
+  }
+
+  const handleSelect = ({ id, value }: { id: number, value: string }) => {
+
+    setTodos((prevTodos) => (
+      prevTodos.map((todo) => (
+        todo.id === id ? { ...todo, isComplete: value === 'yes' ? true : false } : todo
+      ))
+    ))
+
   }
 
   return (
@@ -75,7 +145,7 @@ export default function page() {
       <form onSubmit={handleSubmit}
         className="flex w-full max-w-sm items-center space-x-2 mx-auto my-12">
         <Input type="text" placeholder="What's on your mind?"
-          value={inputValue}
+          value={isEditOpen ? '' : inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
         <Button type="submit" className="bg-indigo-700 hover:bg-indigo-800 text-slate-200 transition-all ease-in-out duration-300 px-6">PUSH</Button>
@@ -94,70 +164,117 @@ export default function page() {
               <div className="p-1">
                 <Card className="flex items-center">
                   <CardContent className="flex items-center justify-left p-6">
-                    <p>{index + 1}. {todo.desc}</p>
+                    <p><span className="select-none">{index + 1}.</span> <span style={todo.isComplete ? { textDecoration: 'line-through' } : {}}>{todo.desc}</span></p>
                   </CardContent>
-                  <div className="absolute right-6 flex items-center gap-1 opacity-90"
+                  <div className="absolute right-2 flex items-center opacity-90 -space-x-2"
                   >
 
-                    <Dialog>
-                      <DialogTrigger asChild>
-                        <span className="cursor-pointer"><FaEdit size="1.15rem" /></span>
-                      </DialogTrigger>
-                      <DialogContent className="sm:max-w-[425px]">
-                        <DialogHeader>
-                          <DialogTitle>Edit profile</DialogTitle>
-                          <DialogDescription>
-                            Make changes to your profile here. Click save when you're done.
-                          </DialogDescription>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                          <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="name" className="text-right">
-                              Name
-                            </Label>
-                            <Input
-                              id="name"
-                              defaultValue="Pedro Duarte"
-                              className="col-span-3"
-                            />
-                          </div>
-                          <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="username" className="text-right">
-                              Username
-                            </Label>
-                            <Input
-                              id="username"
-                              defaultValue="@peduarte"
-                              className="col-span-3"
-                            />
-                          </div>
-                        </div>
-                        <DialogFooter>
-                          <Button type="submit">Save changes</Button>
-                        </DialogFooter>
-                      </DialogContent>
-                    </Dialog>
+                    <div className="flex gap-1">
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <Dialog open={isEditOpen} onOpenChange={setIsEditOpen}>
+                              <DialogTrigger asChild>
+                                <span
+                                  className="cursor-pointer"
+                                  onClick={() => handleEditIcon(todo.id)}
+                                // title="Edit"
+                                ><FaEdit size="1.15rem" />
+                                </span>
+                              </DialogTrigger>
+                              <DialogContent className="sm:max-w-[425px]">
+                                <DialogHeader>
+                                  <DialogTitle>Edit ToDo</DialogTitle>
+                                  <DialogDescription>
+                                    Make changes to your ToDo here. Click save when you're done.
+                                  </DialogDescription>
+                                </DialogHeader>
+                                <div className="grid gap-4 py-4">
+                                  <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="name" className="text-right">
+                                      Description
+                                    </Label>
+                                    <Input
+                                      id="name"
+                                      value={inputValue}
+                                      className="col-span-3"
+                                      onChange={(e) => setInputValue(e.target.value)}
+                                    />
+                                  </div>
+                                  <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="username" className="text-right">
+                                      Completed?
+                                    </Label>
+                                    <Select onValueChange={(value) => handleSelect({ id: todo.id, value })}>
+                                      <SelectTrigger className="col-span-3">
+                                        <SelectValue placeholder="Select a status" />
+                                      </SelectTrigger>
+                                      <SelectContent>
+                                        <SelectGroup>
+                                          <SelectItem value="yes">Yes</SelectItem>
+                                          <SelectItem value="no">No</SelectItem>
+                                        </SelectGroup>
+                                      </SelectContent>
+                                    </Select>
+                                  </div>
+                                </div>
+                                <DialogFooter>
+                                  <Button
+                                    onClick={() => handleEditSave(todo.id)}
+                                  >Save changes</Button>
+                                </DialogFooter>
+                              </DialogContent>
+                            </Dialog>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>Edit</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
 
-                    <AlertDialog>
-                      <AlertDialogTrigger asChild>
-                        <span className="cursor-pointer"><IoMdTrash size="1.3rem" /></span>
-                      </AlertDialogTrigger>
-                      <AlertDialogContent>
-                        <AlertDialogHeader>
-                          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                          <AlertDialogDescription>
-                            This action cannot be undone. This will permanently delete your
-                            ToDo and remove it from our servers.
-                          </AlertDialogDescription>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter>
-                          <AlertDialogCancel>Cancel</AlertDialogCancel>
-                          <AlertDialogAction
-                            onClick={() => deleteTodo(todo.id)}
-                          >Continue</AlertDialogAction>
-                        </AlertDialogFooter>
-                      </AlertDialogContent>
-                    </AlertDialog>
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <AlertDialog>
+                              <AlertDialogTrigger asChild>
+                                <span
+                                  className="cursor-pointer"
+                                // title="Delete"
+                                ><IoMdTrash size="1.3rem" />
+                                </span>
+                              </AlertDialogTrigger>
+                              <AlertDialogContent>
+                                <AlertDialogHeader>
+                                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                                  <AlertDialogDescription>
+                                    This action cannot be undone. This will permanently delete your
+                                    ToDo and remove it from our servers.
+                                  </AlertDialogDescription>
+                                </AlertDialogHeader>
+                                <AlertDialogFooter>
+                                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                  <AlertDialogAction
+                                    onClick={() => deleteTodo(todo.id)}
+                                  >Continue</AlertDialogAction>
+                                </AlertDialogFooter>
+                              </AlertDialogContent>
+                            </AlertDialog>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>Delete</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </div>
+
+                    <div className="-rotate-90"
+                      style={{ rotate: '180deg' }}
+                    >
+                      <Switch
+                        checked={todo.isComplete}
+                        onCheckedChange={() => handleSwitch(todo.id)}
+                      />
+                    </div>
 
                   </div>
                 </Card>
@@ -167,7 +284,7 @@ export default function page() {
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
-      </Carousel>
+      </Carousel >
     </main >
   )
 }
